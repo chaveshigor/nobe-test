b1fbf3a62c497b3ba7e1e00730f719bcb10ba9cc
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-10 19:53:38 -0300

    Add model tests

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe BankAccount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:bank_account) { create(:bank_account, :with_user) }
+
+  describe 'Assossiations' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'Validations' do
+    subject { bank_account }
+
+    it { should validate_uniqueness_of(:account_number) }
+    it { should validate_numericality_of(:balance).is_greater_than_or_equal_to(0) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Assossiations' do
+    it { should have_one(:bank_account) }
+  end
+
+  describe 'Validations' do
+    subject {
+      User.new(
+        first_name: 'Son',
+        last_name: 'Goku',
+        phone: '21789654123',
+        password: 'kamehameha',
+        email: 'goku@test.com'
+      )
+    }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:phone) }
+  end
 end
