a4513b711271f0c5652f28b5143de309b0326f1c
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-10 16:48:34 -0300

    Add docker config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 rm -f /nobe-test/tmp/pids/server.pid
 bundle install
 # rails db:drop
-rails db:create
+# rails db:create
 rails db:migrate
 # rails db:seed
 rails server --binding 0.0.0.0 --port $APPLICATION_PORT
