a4fd59cd982805845a3ab80a2d766a9e9b4f282d
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-10 20:31:21 -0300

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :bank_account
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+
+  def full_name
+    "#{first_name.titleize} #{last_name.titleize}"
+  end
 end
