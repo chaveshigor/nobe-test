61feb37cf561af80a4db9b796e557df7d80c5948
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-10 20:30:38 -0300

    Add relation between bank_account and transactions

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount < ApplicationRecord
+  before_validation :generate_bank_account_number, on: :save
+
   belongs_to :user
+
+  has_many :sended, class_name: 'Transaction', foreign_key: 'account_sender_id'
+  has_many :received, class_name: 'Transaction', foreign_key: 'account_receiver_id'
+
+  validates :account_number, uniqueness: true
+  validates :balance, numericality: { greater_than_or_equal_to: 0 }
+
+  private
+
+  def generate_bank_account_number
+    number_of_digits = 5
+
+    self.account_number = SecureRandom.random_number(10**number_of_digits)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,4 @@
 class Transaction < ApplicationRecord
-  belongs_to :account_sender
-  belongs_to :account_receiver
+  belongs_to :account_sender, class_name: 'BankAccount', foreign_key: 'account_sender_id'
+  belongs_to :account_receiver, class_name: 'BankAccount', foreign_key: 'account_receiver_id'
 end
