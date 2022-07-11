0b713de8cd42a128638f23194f4ea15176a29be8
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-11 05:52:28 -0300

    Add transaction model tests

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Assossiations' do
+    it { should belong_to(:account_sender) }
+    it { should belong_to(:account_receiver) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:amount) }
+    it { should validate_numericality_of(:amount).is_greater_than_or_equal_to(0) }
+
+    it { should define_enum_for(:transaction_type).with_values(DRAW: 0, DEPOSIT: 1, SEND: 2) }
+    it { should validate_presence_of(:transaction_type) }
+  end
 end
