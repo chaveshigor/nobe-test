eb27bf9d6a4dc2b634e6ebd56451eecaaea0e1b3
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-11 13:29:40 -0300

    Add transactions method

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,13 +1,19 @@
 class BankAccount < ApplicationRecord
-  before_validation :generate_bank_account_number, on: :save
+  before_create :generate_bank_account_number
 
   belongs_to :user
-
   has_many :sended, class_name: 'Transaction', foreign_key: 'account_sender_id'
   has_many :received, class_name: 'Transaction', foreign_key: 'account_receiver_id'
 
   validates :account_number, uniqueness: true
   validates :balance, numericality: { greater_than_or_equal_to: 0 }
+
+  def transactions
+    Transaction.where(account_sender_id: id)
+               .or(Transaction.where(account_receiver_id: id))
+               .distinct
+               .order(created_at: :desc)
+  end
 
   private
 
