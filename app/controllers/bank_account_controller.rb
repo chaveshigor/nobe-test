9847c63765c76c9f8af37af221883bcec1d1b1de
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-11 11:41:38 -0300

    Add bank account show view

--- app/controllers/bank_account_controller.rb
+++ app/controllers/bank_account_controller.rb
@@ -1,5 +1,9 @@
 class BankAccountController < ApplicationController
-  def index
-    
+  before_action :authenticate_user!
+
+  def show
+    user_id = params[:id]
+
+    @bank_account = User.find(user_id).bank_account
   end
 end
