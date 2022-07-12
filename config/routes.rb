ed307449382b82b8f008f965a17f01f865eb5c75
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-12 18:56:51 -0300

    Add transactions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'show_account', to: 'bank_account#show_account'
-  resources :bank_account
+  resources :transactions
 
   root 'bank_account#show_account'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
