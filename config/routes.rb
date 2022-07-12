302b8adf074e8e64212d4659975936e553a64f40
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-12 05:50:39 -0300

    Add show account route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  get 'show_account', to: 'bank_account#show_account'
+  resources :bank_account
+
+  root 'bank_account#show_account'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
