47746440509936e37af7f1876f2986f1168f38bf
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-07-13 07:06:34 -0300

    Fix date filter

--- app/controllers/bank_account_controller.rb
+++ app/controllers/bank_account_controller.rb
@@ -5,10 +5,8 @@ class BankAccountController < ApplicationController
 
   def show_account
     user_id = current_user.id
-    p '-------------koé', transaction_params
     @bank_account = User.find(user_id).bank_account
-    p params[:'/show_account'][:initial_date]
-    if params[:'/show_account'][:initial_date] || params[:'/show_account'][:final_date]
+    if params[:'/show_account'] && (params[:'/show_account'][:initial_date] || params[:'/show_account'][:final_date])
       @transactions = filter_by_date
     else
       @transactions = nil
@@ -18,8 +16,8 @@ class BankAccountController < ApplicationController
   private
 
   def filter_by_date
-    initial_date = params[:'/show_account'][:initial_date] || @bank_account.created_at
-    final_date = params[:'/show_account'][:final_date] || Time.zone.today
+    initial_date = params[:'/show_account'][:initial_date].blank? ? @bank_account.created_at : params[:'/show_account'][:initial_date]
+    final_date = params[:'/show_account'][:final_date].blank? ? Time.zone.now : params[:'/show_account'][:final_date]
 
     @bank_account.transactions.where('created_at >= ? AND created_at <= ?', initial_date, final_date)
   end
